fb2982409842434f32dbd8396a3baeaa78c5a0d0
yjiao <yjiao@g2link.cn>
2019-08-02 15:09:21 +0800

    init

---
+++ src/main/java/com/yang/echo/server/EchoServer.java
@@ -0,0 +1,66 @@
+package com.yang.echo.server;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+import java.net.InetSocketAddress;
+
+/**
+ * @author JiaoYang
+ * @date 2019/7/25
+ */
+public class EchoServer {
+
+    private final int port;
+
+    public EchoServer(int port) {
+        this.port = port;
+    }
+
+    public static void main(String[] args) throws Exception {
+        if (args.length != 1) {
+            System.err.println("Usage: " + EchoServer.class.getSimpleName() + " <port>");
+            return;
+        }
+        // 设置端口值（抛出一个 NumberFormatException 如果该端口参数的格式不正确）
+        int port = Integer.parseInt(args[0]);
+        // 呼叫服务器的 start() 方法
+        new EchoServer(port).start();
+    }
+
+    public void start() throws Exception {
+        // 创建 EventLoopGroup
+        NioEventLoopGroup group = new NioEventLoopGroup();
+
+        try {
+            // 创建 ServerBootstrap
+            ServerBootstrap bootstrap = new ServerBootstrap();
+            bootstrap.group(group)
+                    // 指定使用 NIO 的传输 Channel
+                    .channel(NioServerSocketChannel.class)
+                    // 设置 socket 地址使用所选的端口
+                    .localAddress(new InetSocketAddress(port))
+                    // 添加 EchoServerHandler 到 Channel 的 ChannelPipeline
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        public void initChannel(SocketChannel ch) {
+                            ch.pipeline().addLast(new EchoServerHandler());
+                        }
+                    });
+            // 绑定的服务器;sync 等待服务器关闭
+            ChannelFuture f = bootstrap.bind().sync();
+            System.out.println(EchoServer.class.getName() + " started and listen on " + f.channel().localAddress());
+            // 关闭 channel 和 块，直到它被关闭
+            f.channel().closeFuture().sync();
+        } finally {
+            // 关闭 EventLoopGroup，释放所有资源。
+            group.shutdownGracefully().sync();
+        }
+
+    }
+
+}
---
+++ src/main/java/com/yang/echo/test/Test1.java
@@ -0,0 +1,22 @@
+package com.yang.echo.test;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelFactory;
+import io.netty.channel.internal.ChannelUtils;
+
+import java.nio.channels.Channels;
+
+/**
+ * @author JiaoYang
+ * @date 2019/7/31
+ */
+public class Test1 {
+
+    public static void main(String[] args) {
+       // Channel channel = Channels.
+        // ByteBuf byteBuf = Unpooled.
+    }
+
+}
